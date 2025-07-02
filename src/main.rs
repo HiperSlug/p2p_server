8de65b997529ceb06ce97bd05ff52560d1c98505
Raphael <raphael.larsen@proton.me>
2025-07-02 12:10:08 -0400

    Basic client pinging example.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
+use std::time::Duration;
+
+use tokio::time::sleep;
 use tonic::{transport::Server, Request, Response, Status};
 use puncher::hello_service_server::{HelloService, HelloServiceServer};
 use puncher::{HelloRequest, HelloResponse};
+
+use crate::puncher::hello_service_client::HelloServiceClient;
 
 pub mod puncher {
 	tonic::include_proto!("puncher");
@@ -21,8 +27,25 @@ impl HelloService for MyHelloService {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-	let addr = "[::1]:50051".parse()?;
+	let addr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::LOCALHOST, 3000));
 	let hello_service = MyHelloService::default();
+
+    tokio::spawn(async move {
+        let mut client = HelloServiceClient::connect("https://localhost:3000").await.unwrap();
+        
+        
+        loop {
+            sleep(Duration::from_secs(1)).await;
+            let request = tonic::Request::new(HelloRequest {
+                name: "World".into(),
+            });
+
+            let response = client.say_hello(request).await.unwrap();
+
+            println!("Response={response:?}");
+
+        }
+    });
 
 	Server::builder()
 		.add_service(HelloServiceServer::new(hello_service))
