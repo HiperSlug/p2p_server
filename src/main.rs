82768af0062b8c4a37a2ad3f52bbce9a5e222f71
Raphael <raphael.larsen@proton.me>
2025-07-02 14:33:56 -0400

    A lil more testing/example code for the client.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,7 @@
+use nat_puncher::server;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-	// let addr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::LOCALHOST, 3000));
-	// let hello_service = MyHelloService::default();
-
-    // tokio::spawn(async move {
-    //     let mut client = HelloServiceClient::connect("https://localhost:3000").await.unwrap();
-        
-        
-    //     loop {
-    //         sleep(Duration::from_secs(1)).await;
-    //         let request = tonic::Request::new(HelloRequest {
-    //             name: "World".into(),
-    //         });
-
-    //         let response = client.say_hello(request).await.unwrap();
-
-    //         println!("Response={response:?}");
-
-    //     }
-    // });
-
-	// Server::builder()
-	// 	.add_service(HelloServiceServer::new(hello_service))
-	// 	.serve(addr)
-	// 	.await?;
-
+async fn main() -> anyhow::Result<()> {
+    server::run().await?;
 	Ok(())
 }
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,7 @@
-use crate::puncher::{self, puncher_service_server::PuncherService, AddListingRequest, AddListingResult, GetListingsResult, RemoveListingRequest, RemoveListingResult};
-use tonic::{Request, Response, Status};
+use std::{net::SocketAddr, time::Duration};
+use crate::puncher::{self, puncher_service_client::PuncherServiceClient, puncher_service_server::{PuncherService, PuncherServiceServer}, AddListingRequest, AddListingResult, GetListingsResult, ListingNoId, RemoveListingRequest, RemoveListingResult};
+use tokio::time::sleep;
+use tonic::{transport::Server, Request, Response, Status};
 
 #[derive(Default)]
 pub struct PuncherServer;
@@ -26,4 +28,40 @@ impl PuncherService for PuncherServer {
     ) -> Result<Response<GetListingsResult>, Status> {
         Err(Status::aborted(""))
     }
+}
+
+pub async fn run() -> anyhow::Result<()> {
+	let addr: SocketAddr = "127.0.0.1:3000".parse().unwrap();
+	let server = PuncherServer::default();
+
+    tokio::spawn(async move {
+        let mut client = PuncherServiceClient::connect("https://localhost:3000").await.unwrap();
+        
+        
+        loop {
+            sleep(Duration::from_secs(1)).await;
+            let request = tonic::Request::new(AddListingRequest {
+                listing: Some(ListingNoId {
+					name: "Raphael".to_string(),
+				}),
+            });
+
+            let response = match client.add_listing(request).await {
+				Ok(resp) => resp,
+				Err(e) => {
+					eprintln!("Err: {e}");
+					continue;
+				}
+			};
+
+            println!("Response = {response:?}");
+        }
+    });
+
+	Server::builder()
+		.add_service(PuncherServiceServer::new(server))
+		.serve(addr)
+		.await?;
+	
+	Ok(())
 }
