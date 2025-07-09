617a87637ef56efe5448db143c9547148c858c7b
Raphael <raphael.larsen@proton.me>
2025-07-09 17:55:19 -0400

    Changed address.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ use nat_puncher::server;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    server::run("127.0.0.1:3000".parse().unwrap()).await?;
+    server::run("0.0.0.0:3000".parse().unwrap()).await?;
 	Ok(())
 }
